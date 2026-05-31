929f7f9aa74068314041e192d2aaddcd4433f4e6
rbrock44 <rbrock444@gmail.com>
2026-05-31 16:16:19 -0500

    first round of refactor

--- src/app/components/schedule/schedule.component.ts
+++ src/app/components/schedule/schedule.component.ts
@@ -262,12 +262,12 @@ export class ScheduleComponent implements OnInit {
       {id: undefined, date: '2024-08-28', startTime: '09:30', endTime: '10:30', title: 'Sprint Planning', person: 'Mitchelle'},
     ];
 
-    const apiMeetings = await getAllMeetings();
+    // const apiMeetings = await getAllMeetings();
 
     this.allMeetings = [
       // HELPFUL: add fake meetings if running locally and need to see some
-      // ...fakeMeetings,
-      ...apiMeetings
+      ...fakeMeetings,
+      // ...apiMeetings
     ];
 
     this.filterMeetings();
